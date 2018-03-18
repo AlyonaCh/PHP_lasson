225fc9d2f19b5d6f50518b64252571653bd6e98c
AlyonaCh <fresh1210@yandex.ru>
2018-03-18 16:40:32 +0500

    first commit

--- Product/Start.php
+++ Product/Start.php
@@ -1,15 +1,15 @@
 <?php
 function myAutoload ($className) {
-  $filePath = $className . '.php';
+  $filePath = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
   if (file_exists($filePath)) {
    include "$filePath";
   }
 }
 spl_autoload_register ('myAutoload');
-$tomato = new \Vegetables\cl ('Tomato', 200);
-$apple = new \Fruit\cl ('Apple', 29);
-$jacket = new \Clothest\cl ('Jacket', 200);
-$korzina = new \Cart\cl ();
+$tomato = new Vegetables\cl ('Tomato', 200);
+$apple = new Fruit\cl ('Apple', 29);
+$jacket = new Clothest\cl ('Jacket', 200);
+$korzina = new Cart\cl ();
 $korzina->addProduct ($tomato);
 $korzina->addProduct ($apple);
 $korzina->addProduct ($jacket);
